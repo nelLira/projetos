ab700148038068cd47bbaac9d9ad37e27cdcc334
nelLira <wflira@gmail.com>
2019-03-18 16:34:54 -0300

    180320191634

--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -19,20 +19,15 @@ public class Lotofacil {
 	//simulacao();
 //	gerarJogos(100,true);
 //	estisticasUltimoSorteio();
- // estatisticasUltimosSorteios(50, true);
+ //estatisticasUltimosSorteios(50, true);
 		
 			List<Jogo> jogos = new ArrayList<Jogo>();
-			jogos.add(new Jogo(Arrays.asList(1, 3, 4, 7, 8, 9, 11, 13, 14, 15, 16, 17, 21, 23, 24)));
-			jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 7, 9, 13, 15, 16, 17, 19, 21, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(2, 3, 6, 9, 11, 12, 13, 15, 16, 17, 19, 22, 23, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(3, 5, 6, 8, 11, 12, 13, 15, 17, 20, 21, 22, 23, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(4, 5, 7, 8, 9, 10, 12, 13, 15, 17, 20, 21, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 3, 5, 9, 10, 11, 12, 13, 15, 17, 18, 19, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(3, 4, 5, 6, 8, 12, 13, 15, 17, 18, 19, 22, 23, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 4, 5, 7, 8, 9, 10, 13, 15, 16, 17, 18, 20, 21, 24)));
-			jogos.add(new Jogo(Arrays.asList(2, 5, 6, 7, 8, 13, 15, 16, 17, 19, 20, 21, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 3, 5, 6, 7, 10, 13, 15, 16, 17, 18, 19, 22, 23, 24)));
-			jogos.add(new Jogo(Arrays.asList(2, 4, 5, 6, 8, 9, 10, 12, 15, 16, 17, 21, 22, 23, 24)));
+			jogos.add(new Jogo(Arrays.asList(1, 3, 4, 6, 7, 8, 11, 12, 17, 19, 21, 22, 23, 24, 25)));
+			jogos.add(new Jogo(Arrays.asList(1, 2, 3, 8, 10, 11, 12, 14, 16, 17, 18, 21, 22, 24, 25)));
+			jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 9, 12, 13, 16, 17, 19, 21, 22, 23, 24, 25)));
+			jogos.add(new Jogo(Arrays.asList(2, 3, 6, 7, 8, 11, 12, 14, 16, 19, 21, 22, 23, 24, 25)));
+			jogos.add(new Jogo(Arrays.asList(1, 3, 5, 8, 9, 10, 11, 12, 14, 16, 18, 21, 22, 23, 24)));
+			jogos.add(new Jogo(Arrays.asList(2, 3, 5, 6, 7, 8, 9, 12, 15, 16, 17, 20, 22, 24, 25)));
 			consultaSorteio(jogos);
 	}
 
