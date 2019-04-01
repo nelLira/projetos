aa1c8066b4f01792ed8d8c691687b4363c8a2f9f
P_991129 <welington.lira@camara.leg.br>
2019-04-01 16:17:34 -0300

    010420191617

--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -22,13 +22,11 @@ public class Lotofacil {
 //estatisticasUltimosSorteios(50, true);
 		
 			List<Jogo> jogos = new ArrayList<Jogo>();
-			jogos.add(new Jogo(Arrays.asList(1, 4, 5, 7, 8, 9, 11, 12, 13, 15, 18, 20, 21, 22, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 2, 4, 6, 7, 8, 10, 11, 12, 14, 17, 21, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 2, 5, 6, 7, 8, 9, 12, 14, 16, 17, 19, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 7, 9, 12, 14, 16, 17, 19, 21, 22, 23, 25)));
-			jogos.add(new Jogo(Arrays.asList(1, 2, 4, 6, 7, 8, 12, 13, 14, 15, 18, 19, 22, 24, 25)));
-			jogos.add(new Jogo(Arrays.asList(2, 3, 4, 7, 9, 10, 12, 15, 17, 19, 20, 21, 22, 23, 25)));
-			jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 7, 9, 10, 12, 14, 15, 17, 18, 19, 22, 23)));
+			jogos.add(new Jogo(Arrays.asList(1, 5, 6, 7, 8, 10, 11, 12, 14, 15, 17, 20, 22, 23, 24)));
+			jogos.add(new Jogo(Arrays.asList(1, 4, 5, 6, 9, 12, 13, 15, 17, 18, 19, 20, 23, 24, 25)));
+			jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 9, 11, 12, 13, 17, 19, 20, 21, 22, 23, 25)));
+			jogos.add(new Jogo(Arrays.asList(1, 2, 4, 6, 7, 10, 11, 12, 14, 17, 18, 19, 20, 22, 23)));
+			jogos.add(new Jogo(Arrays.asList(1, 2, 5, 6, 7, 8, 9, 11, 12, 14, 16, 17, 19, 23, 25)));
 			consultaSorteio(jogos);
 	}
 
