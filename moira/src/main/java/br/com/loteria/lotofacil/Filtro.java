01cc0cedafd2f5f9ad0a672b1c0210cd4c1d4a0a
P_991129 <welington.lira@camara.leg.br>
2019-05-09 16:10:52 -0300

    090520191610

--- moira/src/main/java/br/com/loteria/lotofacil/Filtro.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Filtro.java
@@ -80,24 +80,24 @@ public class Filtro {
 		
 		
 		repetidosJogoAnterior(Arrays.asList(8));
-		pares(Arrays.asList(6,7,8,9)); // 5a9; // {3=3, 4=21, 5=117, 6=339, 7=516, 8=429, 9=182,  10=41, 11=5} 
-		primos(Arrays.asList(5,6,7)); // 4a7 {2=5, 3=76, 4=278, 5=520, 6=451, 7=257, 8=62, 9=4} 
-		sequenciaDeFibonacci(Arrays.asList(2,3,5,6));// 2a6 {1=8, 2=94, 3=351, 4=610, 5=479, 6=200, 7=19}
-		quadrado(Arrays.asList(8,9,10,11)); // 8a11 {6=2, 7=52,8=199, 9=450, 10=483, 11=261, 12=88,13=6} 
+		pares(Arrays.asList(5,6,7,8,9)); // 5a9; // {3=3, 4=21, 5=117, 6=339, 7=516, 8=429, 9=182,  10=41, 11=5} 
+		primos(Arrays.asList(4,5,6,7)); // 4a7 {2=5, 3=76, 4=278, 5=520, 6=451, 7=257, 8=62, 9=4} 
+		sequenciaDeFibonacci(Arrays.asList(2,3,4,5));// 2a6 {1=8, 2=94, 3=351, 4=610, 5=479, 6=200, 7=19}
+		quadrado(Arrays.asList(8,10,11)); // 8a11 {6=2, 7=52,8=199, 9=450, 10=483, 11=261, 12=88,13=6} 
 		multiplosDeTres(Arrays.asList(3,4,5,7)); // 3a7 {1=1, 2=33, 3=193, 4=430, 5=541,  6=317,7=111,8=14}
-		dezMais(Arrays.asList(4,6,7,8)); // 4a8 {2=2, 3=24, 4=140, 5=387, 6=567, 7=415, 8=165, 9=24, 10=3} 
-		numerosImportantes(Arrays.asList(3,4,5,6,8));// 3a8//{1=2, 2=12, 3=84, 4=303, 5=538, 6=549, 7=254, 8=50, 9=3} 
+		dezMais(Arrays.asList(5,6,7,8)); // 4a8 {2=2, 3=24, 4=140, 5=387, 6=567, 7=415, 8=165, 9=24, 10=3} 
+		numerosImportantes(Arrays.asList(3,4,5,6));// 3a8//{1=2, 2=12, 3=84, 4=303, 5=538, 6=549, 7=254, 8=50, 9=3} 
 		QtdFiltros(Arrays.asList(0,1,2,3,4,5));
 		
 		linha(Arrays.asList(1, 2, 3, 4, 5),
 			  Arrays.asList(1, 2, 3, 4, 5),  
-			  Arrays.asList(1, 2, 3, 4, 5), 
+			  Arrays.asList(1, 2, 4, 5), 
 			  Arrays.asList(1, 2, 3, 4, 5),
 			  Arrays.asList(1, 2, 3, 4, 5));
 		
-		coluna(Arrays.asList(2, 3, 4, 5),
+		coluna(Arrays.asList(1, 2, 3, 4, 5),
 			   Arrays.asList(1, 2, 3, 4, 5),
-			   Arrays.asList(1, 2, 3, 4, 5),
+			   Arrays.asList(2, 3, 4, 5),
 			   Arrays.asList(1, 2, 3, 4), 
 			   Arrays.asList(2, 3, 4, 5));
 		
--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -17,19 +17,17 @@ public class Lotofacil {
 	public static void main(String[] args) throws FileNotFoundException, IOException { 
 
 	//	simulacao();
-		//gerarJogos(200,true);
+	//	gerarJogos(200,true);
 		// estisticasUltimoSorteio();
-		estatisticasUltimosSorteios(1, true);
+	//	estatisticasUltimosSorteios(50, true);
 
-//		List<Jogo> jogos = new ArrayList<Jogo>();
-//		jogos.add(new Jogo(Arrays.asList(1, 5, 8, 10, 11, 12, 13, 15, 17, 18, 19, 21, 22, 24, 25)));
-//		jogos.add(new Jogo(Arrays.asList(2, 5, 9, 10, 11, 12, 14, 15, 16, 17, 19, 21, 22, 23, 24)));
-//		jogos.add(new Jogo(Arrays.asList(1, 3, 6, 8, 10, 11, 13, 14, 17, 18, 20, 21, 22, 23, 25)));
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 4, 5, 8, 10, 12, 13, 15, 16, 17, 21, 23, 24, 25)));
-//		jogos.add(new Jogo(Arrays.asList(1, 5, 7, 8, 10, 12, 13, 15, 17, 20, 21, 22, 23, 24, 25)));
-//		jogos.add(new Jogo(Arrays.asList(2, 4, 6, 7, 9, 10, 11, 15, 16, 18, 19, 20, 21, 23, 25)));
-//		jogos.add(new Jogo(Arrays.asList(2, 3, 5, 7, 10, 11, 12, 14, 15, 16, 17, 19, 20, 22, 24)));
-//		consultaSorteio(jogos);
+		List<Jogo> jogos = new ArrayList<Jogo>();
+		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 6, 8, 9, 10, 12, 15, 16, 19, 20, 23, 25)));
+		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 9, 13, 14, 16, 18, 19, 20, 22, 23, 25)));
+		jogos.add(new Jogo(Arrays.asList(1, 4, 5, 6, 8, 10, 11, 15, 17, 18, 19, 20, 22, 24, 25)));
+		jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 9, 11, 13, 14, 15, 17, 19, 20, 21, 22, 25)));
+		jogos.add(new Jogo(Arrays.asList(1, 4, 5, 7, 9, 10, 11, 14, 16, 17, 18, 20, 23, 24, 25)));
+		consultaSorteio(jogos);
 		
 
 
