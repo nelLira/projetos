31742b05c72bb1e0d807304bcb210e914e73a144
P_991129 <welington.lira@camara.leg.br>
2019-08-23 14:43:08 -0300

    230820191443

--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -17,21 +17,25 @@ public class Lotofacil {
 	public static void main(String[] args) throws FileNotFoundException, IOException { 
 		
 	//  simulacao(); //simulação com 9 -> 427 de 613 [19/06/2019]
-	//	gerarJogos(200,true);
+		gerarJogos(200,true);
 	//	estisticasUltimoSorteio();
-		estatisticasUltimosSorteios(1, true);
+		//estatisticasUltimosSorteios(50, true);
 	 
 //	
 // 		List<Jogo> jogos = new ArrayList<Jogo>();
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 4, 5, 6, 7, 8, 10, 12, 15, 16, 17, 19, 21, 22)));
-//		jogos.add(new Jogo(Arrays.asList(2, 3, 4, 5, 7, 8, 9, 10, 11, 13, 18, 20, 21, 23, 25)));11
-//		jogos.add(new Jogo(Arrays.asList(2, 4, 5, 6, 7, 8, 10, 11, 12, 17, 18, 19, 20, 21, 23)));11
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 17, 18, 20, 21, 23)));11
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 21, 23, 25)));
-//		jogos.add(new Jogo(Arrays.asList(2, 3, 6, 7, 10, 12, 13, 14, 16, 17, 19, 20, 21, 22, 25)));
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 5, 7, 8, 9, 10, 13, 14, 15, 18, 19, 20, 22, 24)));
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 5, 7, 8, 9, 10, 12, 15, 17, 20, 21, 22, 24)));
-//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 7, 8, 9, 10, 14, 17, 18, 20, 21, 23)));11
+//		jogos.add(new Jogo(Arrays.asList(2, 3, 5, 6, 7, 8, 10, 11, 16, 17, 19, 21, 22, 24, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 9, 10, 12, 14, 15, 16, 19, 20, 21, 24)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 6, 7, 9, 11, 16, 18, 19, 20, 22, 24, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 7, 9, 10, 14, 15, 16, 17, 19, 20, 23, 24, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 7, 8, 9, 13, 17, 18, 19, 20, 22, 24, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 5, 6, 8, 11, 12, 14, 15, 19, 20, 21, 23, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 9, 10, 12, 13, 14, 16, 19, 21, 22, 25)));
+//		jogos.add(new Jogo(Arrays.asList(2, 4, 5, 6, 8, 9, 11, 12, 13, 14, 17, 19, 20, 21, 25)));
+//		jogos.add(new Jogo(Arrays.asList(2, 4, 7, 8, 9, 11, 13, 14, 15, 16, 19, 20, 21, 22, 24)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 5, 6, 7, 8, 10, 11, 15, 17, 18, 19, 22, 24)));
+//		jogos.add(new Jogo(Arrays.asList(2, 4, 6, 7, 9, 10, 11, 13, 14, 15, 19, 20, 21, 23, 24)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 10, 12, 14, 15, 17, 18, 19, 21, 22, 23, 25)));
+//		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 8, 9, 10, 11, 14, 18, 19, 21, 22, 24, 25)));
 //		consultaSorteio(jogos);
 
 
