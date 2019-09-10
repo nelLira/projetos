dc887c988629f788be58ca1b368dbfca79fe48b2
KarWel <KarWel@192.168.0.14>
2019-09-09 21:22:28 -0300

    090920192122

--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -17,13 +17,13 @@ public class Lotofacil {
 	public static void main(String[] args) throws FileNotFoundException, IOException { 
 		
 	//  simulacao(); //simulação com 9 -> 427 de 613 [19/06/2019]
-		gerarJogos(200,true);
+	//	gerarJogos(200,true);
 	//	estisticasUltimoSorteio();
 	//	estatisticasUltimosSorteios(50, true);
 	 
 		//9->31; 8->24; 10->20
 	
-	/*	
+		
  		List<Jogo> jogos = new ArrayList<Jogo>();
  		jogos.add(new Jogo(Arrays.asList(1, 3, 4, 7, 8, 10, 12, 18, 19, 20, 21, 22, 23, 24, 25)));
 		jogos.add(new Jogo(Arrays.asList(2, 5, 8, 9, 10, 11, 12, 13, 14, 15, 17, 20, 21, 22, 24)));
@@ -41,7 +41,7 @@ public class Lotofacil {
 		jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 7, 8, 10, 14, 15, 16, 17, 18, 19, 21, 24)));
 		
 		consultaSorteio(jogos);
-*/
+
 
 	}
 
