b6caf63fb5b8df44919549bef9f429580f5c9822
KarWel <KarWel@WeK-PC>
2020-05-14 12:04:08 -0300

    140520201204

--- moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
+++ moira/src/main/java/br/com/loteria/lotofacil/Lotofacil.java
@@ -17,32 +17,32 @@ public class Lotofacil {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 
 		// simulacao(); //simulação com 9 -> 427 de 613 [19/06/2019]
-		gerarJogos(200,true);
+		//gerarJogos(200,true);
 		// estisticasUltimoSorteio();
-		//estatisticasUltimosSorteios(50,true);
+		estatisticasUltimosSorteios(1,true);
 		//
-	/*	
-	List<Jogo> jogos = new ArrayList<Jogo>();
-	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 6, 7, 10, 11, 13, 14, 15, 16, 18, 19, 22, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 7, 9, 10, 11, 13, 15, 16, 18, 20, 22, 23, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 3, 4, 7, 8, 9, 10, 11, 12, 16, 17, 20, 22, 23, 24)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 8, 9, 10, 13, 15, 17, 19, 20, 22, 23, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 8, 11, 12, 15, 17, 18, 20, 22, 23, 24)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 9, 11, 13, 14, 15, 18, 19, 20, 22, 23, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 7, 9, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(3, 5, 9, 10, 12, 13, 14, 15, 16, 17, 19, 22, 23, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(3, 4, 6, 7, 8, 11, 12, 15, 16, 18, 19, 20, 21, 23, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 9, 10, 11, 14, 16, 17, 18, 19, 21, 22, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 10, 11, 13, 14, 15, 17, 19, 21, 22, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 4, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25)));
-	jogos.add(new Jogo(Arrays.asList(3, 4, 5, 6, 7, 9, 11, 12, 16, 17, 18, 20, 22, 24, 25)));
-	jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 8, 9, 10, 12, 13, 15, 16, 17, 20, 23, 25)));
-	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 7, 8, 9, 10, 12, 15, 18, 19, 20, 23, 25)));
-	jogos.add(new Jogo(Arrays.asList(4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 17, 19, 20, 23, 24)));
-
-
-	consultaSorteio(jogos);
-	*/
+//		
+//	List<Jogo> jogos = new ArrayList<Jogo>();
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 6, 7, 10, 11, 13, 14, 15, 16, 18, 19, 22, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 7, 9, 10, 11, 13, 15, 16, 18, 20, 22, 23, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 3, 4, 7, 8, 9, 10, 11, 12, 16, 17, 20, 22, 23, 24)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 8, 9, 10, 13, 15, 17, 19, 20, 22, 23, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 4, 5, 8, 11, 12, 15, 17, 18, 20, 22, 23, 24)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 9, 11, 13, 14, 15, 18, 19, 20, 22, 23, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 5, 7, 9, 14, 15, 16, 17, 18, 19, 22, 23, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(3, 5, 9, 10, 12, 13, 14, 15, 16, 17, 19, 22, 23, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(3, 4, 6, 7, 8, 11, 12, 15, 16, 18, 19, 20, 21, 23, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 9, 10, 11, 14, 16, 17, 18, 19, 21, 22, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 10, 11, 13, 14, 15, 17, 19, 21, 22, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 4, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25)));
+//	jogos.add(new Jogo(Arrays.asList(3, 4, 5, 6, 7, 9, 11, 12, 16, 17, 18, 20, 22, 24, 25)));
+//	jogos.add(new Jogo(Arrays.asList(2, 3, 4, 6, 8, 9, 10, 12, 13, 15, 16, 17, 20, 23, 25)));
+//	jogos.add(new Jogo(Arrays.asList(1, 2, 3, 6, 7, 8, 9, 10, 12, 15, 18, 19, 20, 23, 25)));
+//	jogos.add(new Jogo(Arrays.asList(4, 5, 6, 8, 9, 10, 11, 12, 14, 15, 17, 19, 20, 23, 24)));
+//
+//
+//	consultaSorteio(jogos);
+	
 	}
 
 	private static void consultaSorteio(List<Jogo> jogos) throws FileNotFoundException, IOException {
